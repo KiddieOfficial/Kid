fc9e572c16e2ad575d79ac37e1049352c50f60ad
KiddieOfficial <vip.shironome@gmail.com>
2025-12-11 23:36:01 +0700

    Add files via upload

---
+++ api/poll.js
@@ -0,0 +1,10 @@
+const store = require("../lib/donationStore");
+
+export default function handler(req, res) {
+  const universeId = req.query.universeId;
+  const after = req.query.after || "0";
+
+  const list = store.getAfter(universeId, after);
+
+  res.json(list);
+}
